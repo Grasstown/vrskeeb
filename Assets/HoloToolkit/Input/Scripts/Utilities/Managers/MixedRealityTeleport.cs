8894d25a2f001d3bf8f5c347314fb111fecde59f
Kevin Wu <kevinwu2613@gmail.com>
2019-05-06 18:46:21 -0400

    Finalize UI, Need to Fix Strafing

--- Assets/HoloToolkit/Input/Scripts/Utilities/Managers/MixedRealityTeleport.cs
+++ Assets/HoloToolkit/Input/Scripts/Utilities/Managers/MixedRealityTeleport.cs
@@ -173,6 +173,10 @@ namespace HoloToolkit.Unity.InputModule
                 {
                     DoStrafe(Vector3.back * StrafeAmount);
                 }
+                else if (leftY > -0.8 && Math.Abs(leftX) < 0.3)
+                {
+                    DoStrafe(Vector3.forward * StrafeAmount);
+                }adasd
             }
 
             if (EnableRotation && currentPointingSource == null && !fadeControl.Busy)
--- Assets/Scripts/ballmanager.cs
+++ Assets/Scripts/ballmanager.cs
@@ -1,29 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using HoloToolkit.Unity.InputModule;
 using UnityEngine;
 
 public class ballmanager : MonoBehaviour {
 
-    private List<Transform> balls = new List<Transform>();
-    private Transform ballHolder;
-
+    private static Rigidbody rb;
+    private Text scoreText;
+    private static int score;
+    public GameObject hands;
+    
 	// Use this for initialization
 	void Start () {
-        ballHolder = GameObject.Find("Balls").GetComponent<Transform>();
-        foreach (Transform ball in ballHolder)
-        {
-            balls.Add(ball);
-        }
+        scoreText = GameObject.FindObjectOfType<Text>();
+        rb = gameObject.GetComponent<Rigidbody>();
+        score = 0;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        foreach (Transform ball in balls)
+
+    private void Update()
+    {
+        scoreText.text = "Score: " + score;
+        if (score < 0)
         {
-            if (ball.transform.position.y < 0.0f)
-            {
-                ball.SetPositionAndRotation(new Vector3(-0.00999999f, 0.407f, 4.17f), Quaternion.identity);
-            }
+            scoreText.text = "You Lose!";
+            hands.SetActive(false);
+            MixedRealityTeleport.Instance.EnableTeleport = false;
+            MixedRealityTeleport.Instance.EnableRotation = false;
+            MixedRealityTeleport.Instance.EnableStrafe = false;
         }
-	}
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "10":
+                score += 10;
+                break;
+            case "50":
+                score += 50;
+                break;
+            case "100":
+                score += 100;
+                break;
+            default:
+                break;
+        }
+    }
+
+    void FixedUpdate () {
+        if (gameObject.transform.position.y < -0.5)
+        {
+            score -= 10;
+            rb.velocity = rb.angularVelocity = Vector3.zero;
+            rb.GetComponent<Transform>().SetPositionAndRotation(new Vector3(0.0f, 0.6f, 4.27f), Quaternion.identity);
+        }
+    }
 }
